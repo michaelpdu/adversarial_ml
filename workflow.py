0489bbc15689b3710dd29af568cd61b7cb34af8c
Zhengqiang Rao (RD-CN-INTRN) <zhengqiang_rao@tw.trendnet.org>
2018-03-27 11:27:59 +0800

    put round to process_file

--- workflow.py
+++ workflow.py
@@ -32,56 +32,56 @@ class AdversaryWorkflow:
         self.index_ = 1
         self.generate_count_ = 0
 
-    def process_file(self, cpu_index, round, file_path):
-        try:
-            print('>> Attack {}'.format(file_path))
-            # 
-            dest_dir = os.path.abspath(os.path.join(self.config_['common']['generated_dir'], str(os.getpid()), str(round)))
-            if not os.path.exists(dest_dir):
-                os.makedirs(dest_dir)
-            generator = PEGeneratorRandom(self.config_)
-            generator.load_sample(file_path)
-            generator.load_dna()
-            generator.set_dest_dir(dest_dir)
-            cur_count = self.config_['pe_generator_random']['count']
-            generator.generate(cur_count)
-            self.generate_count_ += cur_count
-            # 
-            trendx = TrendXWrapper(self.config_)
-            hcx_path = os.path.join('tools', 'housecallx', 'hcx{}'.format(cpu_index+1))
-            trendx.set_hcx(hcx_path)
-            scores = trendx.scan_pe_dir(dest_dir)
-            
-            for sample_path, value in scores.items():
-                if value[0] < 2:
-                    try:
-                        print("> Sample: {}, Decision: {}, Current Generated Sample Count: {}".format(sample_path,value[0],self.generate_count_))
-                        info('Find non-malicious sample, decision is {}, submit to cuckoo sandbox: {}'.format(value[0], sample_path))
-                        cmd = 'cuckoo submit --timeout 60 {}'.format(sample_path)
-                        info('> ' + cmd)
-                        print('> ' + cmd)
-                        os.system(cmd)
-                    except Exception as e:
-                        print(e)
-                else:
-                    os.remove(sample_path)
-        except Exception as e:
-            print(e)
-        
+    def process_file(self, cpu_index,file_path):
+        round = self.config_['pe_generator_random']['round']
+        for i in range(round):
+            print('CPU index: {}, Current round is {}'.format(cpu_index, i + 1))
+            try:
+                print('>> Attack {}'.format(file_path))
+                #
+                dest_dir = os.path.abspath(os.path.join(self.config_['common']['generated_dir'], str(os.getpid()), str(i)))
+                if not os.path.exists(dest_dir):
+                    os.makedirs(dest_dir)
+                generator = PEGeneratorRandom(self.config_)
+                generator.load_sample(file_path)
+                generator.load_dna()
+                generator.set_dest_dir(dest_dir)
+                cur_count = self.config_['pe_generator_random']['count']
+                generator.generate(cur_count)
+                self.generate_count_ += cur_count
+                #
+                trendx = TrendXWrapper(self.config_)
+                hcx_path = os.path.join('tools', 'housecallx', 'hcx{}'.format(cpu_index+1))
+                trendx.set_hcx(hcx_path)
+                scores = trendx.scan_pe_dir(dest_dir)
+                
+                for sample_path, value in scores.items():
+                    if value[0] < 2:
+                        try:
+                            print("> Sample: {}, Decision: {}, Current Generated Sample Count: {}".format(sample_path,value[0],self.generate_count_))
+                            info('Find non-malicious sample, decision is {}, submit to cuckoo sandbox: {}'.format(value[0], sample_path))
+                            cmd = 'cuckoo submit --timeout 60 {}'.format(sample_path)
+                            info('> ' + cmd)
+                            print('> ' + cmd)
+                            os.system(cmd)
+                        except Exception as e:
+                            print(e)
+                    else:
+                        os.remove(sample_path)
+            except Exception as e:
+                print(e)
+
+            free = check_free_disk('/')
+            if free < 1024:
+                print('[*] CPU index: {}, No enough disk space, sleep 10 minutes!'.format(cpu_index))
+                time.sleep(600)  # sleep 10m
+
     def process_dir(self, cpu_index, dir_path):
         for root, dirs, files in os.walk(dir_path):
             for name in files:
                 mal_sample_path = os.path.join(root, name)
-                round = self.config_['pe_generator_random']['round']
-                print('CPU index: {}, Total round is {}'.format(cpu_index, round))
-                for i in range(round):
-                    print('CPU index: {}, Current round is {}'.format(cpu_index, i+1))
-                    self.process_file(cpu_index, i, mal_sample_path)
-                    # check free disk in root disk
-                    free = check_free_disk('/')
-                    if free < 1024:
-                        print('[*] CPU index: {}, No enough disk space, sleep 10 minutes!'.format(cpu_index))
-                        time.sleep(600) # sleep 10m
+                self.process_file(cpu_index, mal_sample_path)
+
 
 
     def attack(self, cpu_index):
@@ -91,25 +91,17 @@ class AdversaryWorkflow:
         if not os.path.exists(sample_path):
             raise Exception('Cannot find sample path: {}'.format(sample_path))
         if os.path.isdir(sample_path):
-            self.process_dir(cpu_index, sample_path)
+            self.process_dir(cpu_index,sample_path)
         elif os.path.isfile(sample_path):
-            round = self.config_['pe_generator_random']['round']
-            print('CPU index: {}, Total round is {}'.format(cpu_index, round))
-            for i in range(round):
-                print('CPU index: {}, Current round is {}'.format(cpu_index, i + 1))
-                self.process_file(cpu_index,i,sample_path)
-                # check free disk in root disk
-                free = check_free_disk('/')
-                if free < 1024:
-                    print('[*] CPU index: {}, No enough disk space, sleep 10 minutes!'.format(cpu_index))
-                    time.sleep(600)
+            self.process_file(cpu_index,sample_path)
+
 
         else:
             pass
 
     def start(self):
         try:
-            cpu_count = 1
+            cpu_count = multiprocessing.cpu_count()
             # cpu_count = 2
             # 
             p = NoDaemonPool(cpu_count)
